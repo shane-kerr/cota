4605a80160c507cf3139f7612a3238e1a8589e79
Shane Kerr <shane@time-travellers.org>
2014-03-09 02:36:42 +0100

    Introduction dialog now displayed in a handsome scroll.

--- cota.py
+++ cota.py
@@ -1,4 +1,7 @@
 import sys
+import random
+import textwrap
+import textui
 
 import pprint
 
@@ -42,6 +45,7 @@ class dialog:
                 sys.exit(1)
             line_num = line_num + 1
             if s.strip() == '----':
+                msg_txt_paragraphs.append(' '.join(msg_txt_lines))
                 break
             # separate paragraphs by blank lines; we need to do this in 
             # order to handle word wrap and the like properly on 
@@ -51,9 +55,210 @@ class dialog:
                 msg_txt_lines = []
             else:
                 msg_txt_lines.append(s.strip())
-        return (msg_id, '\n\n'.join(msg_txt_paragraphs), line_num)
+        return (msg_id, msg_txt_paragraphs, line_num)
+
+
+class button:
+    def __init__(self, ui, label, attr):
+        self.ui = ui
+        self.label = label
+        self.attr = attr
+        self.selected = False
+    def show(self, x, y):
+        if self.selected:
+            self.ui.write(x, y, " >" + self.label + "< ", self.attr)
+        else:
+            self.ui.write(x, y, "  " + self.label + "  ", self.attr)
+
+# Scroll design found here:
+#     http://www.retrojunkie.com/asciiart/misc/scrolls.htm
+#  ,----------------------------------------------------------,==.
+# /                                                          /__  \
+# \                                                          |(_\ /
+# /                                                          \-`-'
+# >                                                          /
+# }                                                          }
+# |                                                          \
+# \                                                          /
+# }                                                          {
+# \                                                          /
+# }                                                          >
+# |                                                          {____
+# }                                                          |__( \
+# \                                                          \    /
+#  `----------------------------------------------------------`=='
+scroll_top_left = [ 
+    r" ,-",
+    r"/  ",
+    r"\  ",
+    r"/  " ,
+]
+scroll_top_right = [
+    "-,==. ",
+    "/__  \\",
+    "|(_\ /",
+    "\-`-' ",
+]
+scroll_bottom_left = [
+    "|  ",
+    "}  ",
+    "\\  ",
+    " `-",
+]
+scroll_bottom_right = [
+    "{____ ",
+    "|__( \\",
+    "\    /",
+    "-`==' ",
+]
+
+# initialize our scroll edges
+scroll_side_rough_edges = "/>{}|\\"
+scroll_left_edges = [ random.choice(scroll_side_rough_edges) ]
+scroll_right_edges = [ random.choice(scroll_side_rough_edges) ]
+for n in range(500):    # hopefully we won't ever have more than 500 lines...
+    c = scroll_left_edges[-1]
+    while c == scroll_left_edges[-1]:
+        c = random.choice(scroll_side_rough_edges)
+    scroll_left_edges.append(c)
+    c = scroll_right_edges[-1]
+    while c == scroll_right_edges[-1]:
+        c = random.choice(scroll_side_rough_edges)
+    scroll_right_edges.append(c)
+
+# TODO: make scroll get bigger/smaller on top/bottom
+def dialog_scroll(ui, text):
+    """show some dialog in a scroll"""
+    text_ofs = 0
+    but_up = button(ui, " Up ", 
+                ui.color_attr(textui.BLACK, textui.WHITE, textui.NORMAL))
+    but_down = button(ui, "Down", 
+                ui.color_attr(textui.BLACK, textui.WHITE, textui.NORMAL))
+    but_down.selected = True
+    while True:
+        (width, height) = ui.get_screen_size()
+        ui.set_default_color_attr(textui.YELLOW, textui.YELLOW, textui.BOLD)
+        ui.clear()
+        # convert our text to wrapped text
+        lines = textwrap.wrap(text[0], width-13)
+        for paragraph in text[1:]:
+            lines.append("")
+            lines.extend(textwrap.wrap(paragraph, width-13))
+        # show the top
+        wid_l = len(scroll_top_left[0])
+        wid_r = len(scroll_top_right[0])
+        wid_m = width - (wid_l + wid_r) - 1
+        n = 0
+        for txt in scroll_top_left:
+            ui.write(1, n, txt)
+            n = n + 1
+        n = 0
+        for txt in scroll_top_right:
+            ui.write(width-len(txt)-1, n, txt)
+            n = n + 1
+        ui.write(wid_l, 0, ("-" * wid_m))
+        for n in range(len(scroll_top_right)-1):
+            ui.write(wid_l, n+1, (" " * wid_m))
+        # display the sides
+        side_rough_edges = "/>{}|\\"
+        ofs = len(scroll_top_left)
+        for n in range(height-ofs-len(scroll_bottom_left)):
+            ui.write(1, ofs+n, scroll_left_edges[n])
+            ui.write(width-7, ofs+n, scroll_right_edges[n])
+        # display the bottom
+        wid_l = len(scroll_bottom_left[0])
+        wid_r = len(scroll_bottom_right[0])
+        wid_m = width - (wid_l + wid_r) - 1
+        n = 0
+        for txt in scroll_bottom_left:
+            ui.write(1, height-len(scroll_bottom_left)+n, txt)
+            n = n + 1
+        n = 0
+        for txt in scroll_bottom_right:
+            ui.write(width-len(txt)-1, height-len(scroll_bottom_right)+n, txt)
+            n = n + 1
+        ui.write(wid_l, height-1, "-" * wid_m)
+        # display the text
+        ui.set_default_color_attr(textui.BLACK, textui.YELLOW, textui.NORMAL)
+        n = 0
+        while ((n+text_ofs) < len(lines)) and ((n+2) < (height-4)):
+            ui.write(4, n+2, lines[n+text_ofs])
+            n = n + 1
+        # display our buttons
+        if text_ofs > 0:
+            but_up.show(width-27, height-3)
+        if (n+text_ofs) < len(lines):
+            text_done = False
+            but_down.label = "Down"
+        else:
+            text_done = True
+            but_down.label = "DONE"
+        but_down.show(width-17, height-3)
+        ui.cursor_position(width-1, height-1)
+        input_event = ui.get_input()
+        if input_event.event_type == "keyboard":
+            if input_event.key in (ord('\n'), ord(' ')) and but_down.selected:
+                if text_done:
+                    break
+                else:
+                    text_ofs = text_ofs + 1
+            if input_event.key == textui.KEY_DOWN:
+                if not text_done:
+                    text_ofs = text_ofs + 1
+            elif (input_event.key == textui.KEY_UP) or \
+                 (input_event.key in (ord('\n'), ord(' ')) and but_up.selected):
+                if text_ofs > 0:
+                    text_ofs = text_ofs - 1
+                if text_ofs == 0:
+                    but_up.selected = False
+                    but_down.selected = True
+            elif input_event.key == ord('\t'):
+                if text_ofs > 0:
+                    but_up.selected = not but_up.selected
+                    but_down.selected = not but_down.selected
+            elif input_event.key == textui.KEY_LEFT:
+                if text_ofs > 0:
+                    but_up.selected = True
+                    but_down.selected = False
+            elif input_event.key == textui.KEY_RIGHT:
+                if text_ofs > 0:
+                    but_up.selected = False
+                    but_down.selected = True
+        elif input_event.event_type == "mouse" and input_event.left_click():
+            if (width - 17 <= input_event.x <= width - 10) and \
+               (input_event.y == height-3):
+                if text_done:
+                    break
+                else:
+                    text_ofs = text_ofs + 1
+                    but_up.selected = False
+                    but_down.selected = True
+            elif (width - 27 <= input_event.x <= width - 20) and \
+                 (input_event.y == height-3):
+                if text_ofs > 0:
+                    text_ofs = text_ofs - 1
+                if text_ofs > 0:
+                    but_up.selected = True
+                    but_down.selected = False
+                else:
+                    but_up.selected = False
+                    but_down.selected = True
+            but_up.show(width-27, height-3)
+
+class game:
+    def __init__(self, debug_log=None):
+        self.dialog = dialog()
+        self.debug_log = debug_log
+    def dbg(self, msg):
+        if self.debug_log:
+            self.debug_log.write(msg)
+
+
+def main_loop(ui, g):
+    ui.clear()
+    dialog_scroll(ui, g.dialog.messages["welcome"])
 
 if __name__ == "__main__":
-    dia = dialog()
-    pprint.pprint(dia.messages)
+    g = game(debug_log=open("cota.log", "w"))
+    textui.invoke(main_loop, g)
     
