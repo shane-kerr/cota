4b853feb045d3abb48cf6200d5673712d0c7fd4d
Shane Kerr <shane@time-travellers.org>
2014-03-10 23:08:42 +0100

    Add support for non-cardinal keymaps

--- keymap.py
+++ keymap.py
@@ -6,4 +6,8 @@ keys_u = (textui.KEY_UP,    ord('k'), ord('8'), ord('w'))
 keys_d = (textui.KEY_DOWN,  ord('j'), ord('2'), ord('s'))
 keys_l = (textui.KEY_LEFT,  ord('h'), ord('4'), ord('a'))
 keys_r = (textui.KEY_RIGHT, ord('l'), ord('6'), ord('d'))
+keys_ul = (textui.KEY_HOME, ord('7'))
+keys_ur = (textui.KEY_PGUP, ord('9'))
+keys_dl = (textui.KEY_END,  ord('1'))
+keys_dr = (textui.KEY_PGDN, ord('3'))
 
