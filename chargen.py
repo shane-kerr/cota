76531fa4a5f95bc8d5577345de67f419c5f3c0c4
Shane Kerr <shane@time-travellers.org>
2014-03-11 00:39:58 +0100

    Added the "personal interest" skills (as a complete hack). Done with character creation!

--- chargen.py
+++ chargen.py
@@ -141,11 +141,17 @@ def background_selection(ui):
             ui.scroll_up(longest_name+6, 2, width-1, height-2, height-4)
             choice = new_choice
 
-def show_skill_list(ui, width, height, ofs, cur, skill_list, pc):
+def show_skill_list(ui, width, height, ofs, cur, skill_list, pc, occupation):
     cur_on_color = ui.color_attr(textui.WHITE, textui.BLACK, textui.BOLD)
-    cur_off_color = ui.color_attr(textui.WHITE, textui.BLACK)
+    if occupation:
+        cur_off_color = ui.color_attr(textui.WHITE, textui.BLACK)
+    else:
+        cur_off_color = cur_on_color
     on_color = ui.color_attr(textui.BLACK, textui.WHITE)
-    off_color = ui.color_attr(textui.BLACK, textui.WHITE, textui.BOLD)
+    if occupation:
+        off_color = ui.color_attr(textui.BLACK, textui.WHITE, textui.BOLD)
+    else:
+        off_color = on_color
 
     visible_height = height - 4
     n = 0
@@ -218,7 +224,7 @@ def do_occupation_skills(ui, bg, pc, skill_list):
             step_txt = "[Step 2 of 3]"
             ui.write(width - len(step_txt) - 1, height - 1, step_txt)
             refresh = False
-        show_skill_list(ui, width, height, ofs, cur, skill_list, pc)
+        show_skill_list(ui, width, height, ofs, cur, skill_list, pc, True)
         input_event = ui.get_input()
         if input_event.event_type == "keyboard":
             if input_event.key in keys_d:
@@ -252,7 +258,6 @@ def do_occupation_skills(ui, bg, pc, skill_list):
                                    free_skill_points)
             elif input_event.key in keys_r:
                 name = skill_list.names[cur]
-                # check for unadded skills
                 if (name not in pc.skills_added) and \
                    (unassigned_skill_cnt > 0) and (free_skill_points > 0):
                     pc.skills_added[name] = True
@@ -290,13 +295,106 @@ def do_occupation_skills(ui, bg, pc, skill_list):
                 return "next"
             elif input_event.key == 27:
                 return "prev"
+        elif input_event.event_type == "resize":
+            refresh = True
         while (cur - ofs) >= (height - 4):
             ofs = ofs + 1
         while (cur - ofs) < 0:
             ofs = ofs - 1
 
-# TODO:
-# mouse
+# XXX: cut & paste hack due to lateness of hour and desire for progress
+# TODO: merge and refactor
+def do_other_skills(ui, bg, pc, skill_list):
+    # count the unassigned skills in the background
+    free_skill_points = pc.INT * 10
+    # set up screen
+    ui.set_default_color_attr(textui.BLACK, textui.WHITE)
+    ui.cursor_visible(False)
+    menu_color = ui.color_attr(textui.BLACK, textui.WHITE)
+    choice_color = ui.color_attr(textui.WHITE, textui.BLACK, textui.BOLD)
+    disabled_color = ui.color_attr(textui.BLACK, textui.WHITE, textui.BOLD)
+    ofs = 0
+    cur = 0
+    refresh = True
+    while True:
+        if refresh:
+            textui.wait_for_minimum_size(ui, 80, 24)
+            ui.clear()
+            (width, height) = ui.get_screen_size()
+            instr_txt = "-=[ ASSIGN YOUR PERSONAL INTEREST SKILLS ]=-"
+            ui.write((width-len(instr_txt))//2, 0, instr_txt)
+            ui.write(2, 2, "Background: %s" % bg.name)
+            ui.write(2, 5, "Free Skill Points:  %3d" % free_skill_points)
+            show_keys_help(ui, width, height, 
+                           ("Esc", "Up", "Down", "Left", "Right", "Enter"))
+            step_txt = "[Step 3 of 3]"
+            ui.write(width - len(step_txt) - 1, height - 1, step_txt)
+            refresh = False
+        show_skill_list(ui, width, height, ofs, cur, skill_list, pc, False)
+        input_event = ui.get_input()
+        if input_event.event_type == "keyboard":
+            if input_event.key in keys_d:
+                if cur < len(skill_list.names)-1:
+                    cur = cur + 1
+                if skill_list.names[cur] == '---':
+                    cur = cur + 1
+            elif input_event.key in keys_u:
+                if cur > 0:
+                    cur = cur - 1
+                if skill_list.names[cur] == '---':
+                    cur = cur - 1
+            elif input_event.key in keys_l:
+                name = skill_list.names[cur]
+                if not pc.skill_levels[name] == pc.skill_defaults[name]:
+                    new_level = pc.skill_levels[name]
+                    new_level = new_level - (new_level % 5)
+                    if new_level == pc.skill_levels[name]:
+                        new_level = new_level - 5
+                    new_level = max(new_level, pc.skill_defaults[name])
+                    points_gained = pc.skill_levels[name] - new_level
+                    pc.skill_levels[name] = new_level
+                    free_skill_points = free_skill_points + points_gained
+                    ui.write(2, 5, "Free Skill Points:  %3d" % 
+                                   free_skill_points)
+            elif input_event.key in keys_r:
+                name = skill_list.names[cur]
+                if free_skill_points > 0:
+                    increase = min(free_skill_points, 5)
+                    new_level = pc.skill_levels[name] + increase
+                    new_level = new_level - (new_level % increase)
+                    points_used = new_level - pc.skill_levels[name]
+                    pc.skill_levels[name] = new_level
+                    free_skill_points = free_skill_points - points_used
+                    ui.write(2, 5, "Free Skill Points:  %3d" % 
+                                   free_skill_points)
+            elif input_event.key in keys_ul:
+                # home
+                cur = 0
+                ofs = 0
+            elif input_event.key in keys_dl:
+                # end
+                cur = len(skill_list.names)-1
+                ofs = cur - (height - 4)
+            elif input_event.key in keys_dr:
+                # pgdn
+                cur = min(cur+height-6, len(skill_list.names)-1)
+                if skill_list.names[cur] == '---':
+                    cur = cur + 1
+            elif input_event.key in keys_ur:
+                # pgup
+                cur = max(cur-(height-6), 0)
+                if skill_list.names[cur] == '---':
+                    cur = cur - 1
+            elif input_event.key == textui.KEY_ENTER:
+                return "next"
+            elif input_event.key == 27:
+                return "prev"
+        elif input_event.event_type == "resize":
+            refresh = True
+        while (cur - ofs) >= (height - 4):
+            ofs = ofs + 1
+        while (cur - ofs) < 0:
+            ofs = ofs - 1
 
 def make_char(ui):
     skill_list = Skills()
@@ -304,6 +402,9 @@ def make_char(ui):
     bg = background_selection(ui)
     while do_occupation_skills(ui, bg, pc, skill_list) == "prev":
         bg = background_selection(ui)
+    while do_other_skills(ui, bg, pc, skill_list) == "prev":
+        while do_occupation_skills(ui, bg, pc, skill_list) == "prev":
+            bg = background_selection(ui)
 
 if __name__ == "__main__":
     textui.invoke(make_char)
