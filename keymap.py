6ec0a0d766ef3b34d13f8170be62a374883c396f
Shane Kerr <shane@time-travellers.org>
2014-03-10 03:25:20 +0100

    Minor change to story text.

---
+++ keymap.py
@@ -0,0 +1,7 @@
+# sets of keys
+# we support the arrow keys, vi-keys, numpad keys, and WASD
+keys_u = (textui.KEY_UP,    ord('k'), ord('8'), ord('w'))
+keys_d = (textui.KEY_DOWN,  ord('j'), ord('2'), ord('s'))
+keys_l = (textui.KEY_LEFT,  ord('h'), ord('4'), ord('a'))
+keys_r = (textui.KEY_RIGHT, ord('l'), ord('6'), ord('d'))
+
