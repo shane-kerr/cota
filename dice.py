b6cff6c401f658f9abda720e1482c18c6740ae6a
Shane Kerr <shane@time-travellers.org>
2014-03-13 22:37:58 +0100

    Add ability to create objects like "2D8" or "d4+1".

--- dice.py
+++ dice.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 random.seed(a=1)
 
@@ -10,4 +11,20 @@ def roll(n, sides, plus=0):
     for m in range(n):
         total = total + dieN(sides)
     return total + plus
+
+class die:
+    def __init__(self, fmt):
+        m = re.match(r'\s*(\d*)D(\d+)\s*([+-]\s*\d+)?\s*$', fmt, re.IGNORECASE)
+        assert(m is not None)
+        if m.group(1) == '':
+            self.n = 1
+        else:
+            self.n = int(m.group(1))
+        self.sides = int(m.group(2))
+        if m.group(3):
+            self.plus = int(m.group(3))
+        else:
+            self.plus = 0
+    def roll(self):
+        return roll(self.n, self.sides, self.plus)
 
