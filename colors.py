923be7c7ba872d8cbd883970a4c6d3980139a7c2
Shane Kerr <shane@time-travellers.org>
2014-03-15 12:34:39 +0100

    Simplify color constants a tiny bit

--- colors.py
+++ colors.py
@@ -1,18 +1,19 @@
 from textui import BLACK, BLUE, CYAN, GREEN, MAGENTA, RED, WHITE, YELLOW
-import textui
+from textui import NORMAL, BOLD
 
 _color_mappings = {
-    "black":        ( BLACK, BLACK, textui.NORMAL ),
-    "blue":         ( BLUE, BLACK, textui.NORMAL ),
-    "cyan":         ( CYAN, BLACK, textui.NORMAL ),
-    "green":        ( GREEN, BLACK, textui.NORMAL ),
-    "magenta":      ( MAGENTA, BLACK, textui.NORMAL ),
-    "red":          ( RED, BLACK, textui.NORMAL ),
-    "white":        ( WHITE, BLACK, textui.BOLD ),
-    "yellow":       ( YELLOW, BLACK, textui.BOLD ),
-    "brown":        ( YELLOW, BLACK, textui.NORMAL ),
-    "dark gray":    ( BLACK, BLACK, textui.BOLD ),
-    "light gray":   ( WHITE, BLACK, textui.NORMAL ),
+    "black":        ( BLACK, BLACK, NORMAL ),
+    "blue":         ( BLUE, BLACK, NORMAL ),
+    "cyan":         ( CYAN, BLACK, NORMAL ),
+    "green":        ( GREEN, BLACK, NORMAL ),
+    "magenta":      ( MAGENTA, BLACK, NORMAL ),
+    "red":          ( RED, BLACK, NORMAL ),
+    "white":        ( WHITE, BLACK, BOLD ),
+    "yellow":       ( YELLOW, BLACK, BOLD ),
+    "brown":        ( YELLOW, BLACK, NORMAL ),
+    "dark gray":    ( BLACK, BLACK, BOLD ),
+    "light gray":   ( WHITE, BLACK, NORMAL ),
+    "bright red":   ( RED, BLACK, BOLD)
 }
 
 def parse_color(s):
